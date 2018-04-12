1bd0f74fd224cf4223993307a4a50959a08e6896
Joyce Chun <wooyangee@gmail.com>
2018-04-12 21:24:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,15 +4,15 @@ class Dog
   end
   
   def name 
-    name
+    @name
   end
   
   def breed(dogs_breed)
-    breed=dogs_breed
+    @breed=dogs_breed
   end
   
   def breed
-    breed
+    @breed
   end
 end
 
